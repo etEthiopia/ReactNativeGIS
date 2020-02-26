26e0c2dda34957bb2e955851b6b4384cd5c17741
etEthiopia <daginegussu@gmail.com>
2020-02-26 17:44:06 +0300

    New goal button with text input side by side

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   const [outputText, setOutputText] = useState("Pre Text");
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button
-        title="Change Text"
-        onPress={() => setOutputText("Text Changed")}
-      />
+    <View style={{ padding: 30 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >
+        <TextInput
+          placeholder="New Goal"
+          style={{
+            width: "80%",
+            borderBottomColor: "black",
+            borderBottomWidth: 1,
+            padding: 2
+          }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
     </View>
   );
 }
