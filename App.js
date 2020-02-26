4a9a2b6f3b335180e728b7c850180b3a05691677
etEthiopia <daginegussu@gmail.com>
2020-02-26 21:44:55 +0300

    goals with a scrollview

--- App.js
+++ App.js
@@ -1,24 +1,43 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView
+} from "react-native";
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
 
-
-  const newGoalInputHandler = enteredText =>{
+  const newGoalInputHandler = enteredText => {
     setEnteredGoal(enteredText);
-  }
+  };
 
   const addGoalBtnHandler = () => {
-    console.log(enteredGoal);
-  }
+    setCourseGoals(currentGoals => [...courseGoals, enteredGoal]);
+  };
 
   return (
     <View style={styles.container}>
-      <View style={styles.addGoal}> 
-        <TextInput onChangeText={newGoalInputHandler} value={enteredGoal} style={styles.newGoal} placeholder="New Goal" />
+      <View style={styles.addGoal}>
+        <TextInput
+          onChangeText={newGoalInputHandler}
+          value={enteredGoal}
+          style={styles.newGoal}
+          placeholder="New Goal"
+        />
         <Button onPress={addGoalBtnHandler} title="ADD" />
       </View>
+      <ScrollView style={styles.goalsContainer}>
+        {courseGoals.map(goal => (
+          <View key={goal} style={styles.goalView}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -29,15 +48,26 @@ const styles = StyleSheet.create({
     paddingVertical: 40,
     paddingHorizontal: 15
   },
-  addGoal:{
+  addGoal: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: 'center'
+    alignItems: "center"
   },
   newGoal: {
     padding: 5,
     width: "80%",
     borderBottomColor: "black",
     borderBottomWidth: 1
+  },
+  goalsContainer: {
+    paddingVertical: 10
+  },
+  goalView: {
+    padding: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginVertical: 5
   }
 });
