108e0d85a2fd2e6c5a65022994eb9470f0e5c979
etEthiopia <daginegussu@gmail.com>
2020-02-29 21:47:28 +0300

    DELETE modified on the goal add and delete app

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,24 +1,49 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableNativeFeedback } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableNativeFeedback,
+  Button
+} from "react-native";
 
 const GoalItem = props => {
   return (
-    <TouchableNativeFeedback onPress={() => props.deleteGoal(props.goal.key)}>
     <View style={styles.goalView}>
-      <Text> {props.goal.value} </Text>
+      <Text style={styles.goalText}> {props.goal.value} </Text>
+      <TouchableNativeFeedback onPress={() => props.deleteGoal(props.goal.key)}>
+        <View
+          
+          style={styles.deleteMainView}
+        ></View>
+      </TouchableNativeFeedback>
     </View>
-    </TouchableNativeFeedback>
   );
 };
 
 const styles = StyleSheet.create({
   goalView: {
-    padding: 10,
+    paddingRight: 0,
     backgroundColor: "#ccc",
     borderColor: "black",
     borderWidth: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "stretch",
     borderRadius: 8,
     marginVertical: 5
+  },
+  goalText: {
+    margin: 10,
+    flex: 5
+  },
+  deleteMainView: {
+    justifyContent: "center",
+    alignItems: "baseline",
+    backgroundColor: "red",
+    flex: 1,
+    borderTopRightRadius: 8,
+    borderBottomRightRadius: 8
   }
 });
 
